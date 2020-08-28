a916ffd5a2c7fb59007517ddc1f18449739f1b82
MiluGames7 <60515754+MiluGames7@users.noreply.github.com>
2020-08-29 01:12:27 +0200

    variables python tutorial
    My first python tutorial, for the variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+print ("Hello World")
+print ("OK, Let's go")
+
+#accounting tasks
+
+print ("Make a calculator in python! :D")
+
+#szam1 = 1. szám szam2 = 2.szám. Elvégezhető műveletek *,+,- és ha jól tudom a / az osztás
+print ("Help: make a variable (pl. num1). it's the first number in then operation")
+
+#num1 = 6 next line num2 = 4 The number entered can be any number, not just the ones listed! Performing the Action: print ("num1 * num2")
+#if we run this command, we are ready.
+
+print ("Thank you for read this tutorial ;, Bye")
+print ("Read my other tutorials on")
