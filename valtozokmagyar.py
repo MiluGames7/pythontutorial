2944b85ab002d7f7c163e946189c3eaf91b5eda5
MiluGames7 <60515754+MiluGames7@users.noreply.github.com>
2020-08-29 10:48:37 +0200

    a változók magyar fordítása
    UPDATE! A Változókról készült tutorialomat lefordítottam magyarra.

---
+++ valtozokmagyar.py
@@ -0,0 +1,16 @@
+print ("Helló Világ")
+print ("Ok menjünk")
+
+#a változók
+
+print ("Végezz el műveleteket a pythonban !:D")
+
+#szam1 = pl.6 szam2 pl.4 print szam1 + szam2 és a művelet kész is van :)
+print ("Súgó: készítsen változót (pl. szam1). ez az első szám az akkori műveletben")
+
+#szam1 = 6 következő sor num2 = 4. A beírt szám tetszőleges szám lehet, nem csak a felsoroltak! A művelet végrehajtása: print("szam11 * szam2")
+#Ha futtatjuk ezt a parancsot, készen állunk.
+
+print ("Köszönöm, hogy elolvasta ezt az útmutatót; Viszlát")
+print ("Olvassa el a többi oktatóanyagomat a github oldalamon https://github.com/MiluGames7/pythontutorial")
+
